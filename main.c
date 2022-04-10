d74edc220babaab4665c68d247a508c3f7b6d9f1
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-04-10 14:42:11 -0500

    Crear proyecto

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Sumo.c
+ *
+ * Created: 10/04/2022 02:33:13 p. m.
+ * Author : ricar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
